4d2a2723382debb3a4605a8e0a863525a52ac3d3
Matheus Queiroz <matheus_giga@hotmail.com>
2022-07-19 17:20:06 -0300

    Sexto requisito finalizado

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import App from '../App';
+import pokemons from '../data';
+import Pokemon from '../components/Pokemon';
+import { PokemonDetails } from '../pages';
+
+describe('Testando o componente PokemonDetails', () => {
+  test('Teste se as informações do pokémon são mostradas na tela', () => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={ history }>
+        <PokemonDetails pokemons={ pokemons } />
+      </Router>,
+    );
+  });
+});
