98874a28c8f38cf224c0251961f995a5e002ca07
Matheus Queiroz <matheus_giga@hotmail.com>
2022-07-19 17:38:42 -0300

    Concluido requisito 6

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -2,18 +2,24 @@ import React from 'react';
 import { screen, render } from '@testing-library/react';
 import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
-import pokemons from '../data';
-import Pokemon from '../components/Pokemon';
-import { PokemonDetails } from '../pages';
 
 describe('Testando o componente PokemonDetails', () => {
   test('Teste se as informações do pokémon são mostradas na tela', () => {
     const history = createMemoryHistory();
     render(
       <Router history={ history }>
-        <PokemonDetails pokemons={ pokemons } />
+        <App />
       </Router>,
     );
+
+    const buttonDetails = screen.getByRole('link', { name: /More details/i });
+
+    userEvent.click(buttonDetails);
+
+    const titleDetails = screen.getByRole('heading',
+      { name: /Pikachu Details/i, level: 2 });
+    expect(titleDetails).toBeDefined();
   });
 });
